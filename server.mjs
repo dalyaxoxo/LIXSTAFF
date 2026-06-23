d29607ebf774a544bf97d4fd28f2d02f79f900fe
dalyaxoxo <96061537+dalyaxoxo@users.noreply.github.com>
2026-06-23 12:10:42 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -4,6 +4,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import sendEvalHandler from './api/send-eval.mjs';
+import addWorkerHandler from './api/add-worker.mjs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,7 +23,7 @@ app.get('/health', (req, res) => res.json({ ok: true }));
 
 // API pour envoyer l'évaluation par mail
 app.post('/api/send-eval', (req, res) => sendEvalHandler(req, res));
-
+app.post('/api/add-worker', (req, res) => addWorkerHandler(req, res));
 // Logs au démarrage
 console.log('ENV:', {
   MAIL_PROVIDER: process.env.MAIL_PROVIDER,
